c9bcb59933d0a85e779216a50692bb8a27d66892
MartiSandoval <martina.sandoval.o@mail.pucv.cl>
2026-05-09 14:45:21 -0400

    nopuedohacerrun

--- src/main/menusTerminal/MenuInicio.java
+++ src/main/menusTerminal/MenuInicio.java
@@ -4,6 +4,10 @@ public class MenuInicio {
     public MenuInicio() {
         System.out.println("===============Netflix===============");
         System.out.println("Seleccione una opción:");
+        System.out.println("1. Ver recomendaciones");
+        System.out.println("2. Seguir viendo");
+        System.out.println("3. Buscar pelicula"); // dentro de esta opción estará filtrar
+        System.out.println("4. Salir del sistema");
     }
 
     public static void main(String[] args) {
